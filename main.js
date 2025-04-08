3f718ec112f38b1ac119ee2d541b47ba22bd1694
Michael Copanos <mcopanos@gmail.com>
2025-04-07 22:05:22 -0500

    added nav bar and styling

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello, world');
+let navLinks = ['home', 'about', 'photos', 'contact'];
+let navBar = document. querySelector('#navBar');
+
+const links = navLinks.map((navLink) => {
+   let link = document.createElement('a');
+   link.href = `#${navLink}`
+   link.innerText = navLink;
+   navBar.appendChild(link);
+})
