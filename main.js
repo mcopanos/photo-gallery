82a3ed3d8f2db3183eeb407c7c63c5583d0f31b4
Michael Copanos <mcopanos@gmail.com>
2025-04-12 23:03:35 -0500

    added and styled home section.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let navLinks = ['home', 'about', 'photos', 'contact'];
 let navBar = document. querySelector('#navBar');
 
+// nav links 
 const links = navLinks.map((navLink) => {
    let link = document.createElement('a');
    link.href = `#${navLink}`
    link.innerText = navLink;
    navBar.appendChild(link);
-})
+});
