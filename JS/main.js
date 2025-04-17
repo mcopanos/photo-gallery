b70d57cccbf5c0d1713c7dc44ac3e6b97d278a4a
Michael Copanos <mcopanos@gmail.com>
2025-04-16 22:52:12 -0500

    added icons and text to cards

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,32 @@ const links = navLinks.map((navLink) => {
    link.innerText = navLink;
    navBar.appendChild(link);
 });
+
+
+/* This is the About Section. */
+
+let icons = ['editing', 'discovery', 'camera']
+let headings = ['Working Hard', 'Searching', 'Our View']
+let pgafs = [
+      'Come see what we have been serving up! we have been working har to bring you quality constent.',
+      'Searching all around the world to big you high quality photos. We want you to feel like you\'re there',
+      'See the world through our view. We find the most beautify landscaps to capture.'
+]
+let aboutCards = document.querySelector('#about');
+
+
+for (let i = 0; i < icons.length; i++) {
+      let li = document.createElement('li');
+      li.classList.add('card');
+      li.innerHTML = `
+            <img src="photos/${icons[i]}.png">
+            <h1>${headings[i]}</h1>
+            <p>${pgafs[i]}</p>`;
+      aboutCards.appendChild(li);
+}
+// let aboutItem = aboutItems.map(item => {
+//    let li = document.createElement('li').classList.add('card');
+//    li.innerText = item;
+//    aboutCards.appendChild(li);
+//    console.log(item)
+// })
